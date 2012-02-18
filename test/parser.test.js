28fa4002f5f97ac3480ef415059e6ceddbcd645e
Michael Williamson <mike@zwobble.org>
2012-02-18 17:52:15 +0000

    Move generic parsing functions into a separate library

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,8 +1,6 @@
 var Parser = require("../lib/parser").Parser;
 var nodes = require("../lib/nodes");
-var parsingTesting = require("./parsing-testing");
-var tokeniser = require("../lib/tokeniser");
-var tokens = tokeniser.tokens;
+var parsingTesting = require("lop").testing;
 var assertIsSuccessWithValue = parsingTesting.assertIsSuccessWithValue;
 var assertIsFailureWithRemaining = parsingTesting.assertIsFailureWithRemaining;
 
