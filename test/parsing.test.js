b1330968d69b3b5994182e4e2af7994df241c322
Michael Williamson <mike@zwobble.org>
2012-02-21 20:47:54 +0000

    Replace parseExpression with more general parse function, expose rules

--- test/parsing.test.js
+++ test/parsing.test.js
@@ -1,55 +1,57 @@
-var Parser = require("../lib/parser").Parser;
-var errors = require("lop").errors;
 var options = require("options");
 var some = options.some;
-var nodes = require("../lib/nodes");
+
+var errors = require("lop").errors;
 var parsingTesting = require("lop").testing;
 var assertIsSuccessWithValue = parsingTesting.assertIsSuccessWithValue;
 var assertIsFailure = parsingTesting.assertIsFailure;
 var assertIsError = parsingTesting.assertIsError;
 
-var parser = new Parser();
+var nodes = require("../lib/nodes");
+var parsing = require("../lib/parsing");
+
+var parser = new parsing.Parser();
 
 exports.canParseBooleanLiteralTrue = function(test) {
-    var result = parser.parseExpression("true");
+    var result = parser.parse(parsing.expression, "true");
     assertIsSuccessWithValue(test, result, nodes.boolean(true));
     test.done();
 };
 
 exports.canParseBooleanLiteralFalse = function(test) {
-    var result = parser.parseExpression("false");
+    var result = parser.parse(parsing.expression, "false");
     assertIsSuccessWithValue(test, result, nodes.boolean(false));
     test.done();
 };
 
 exports.canParseUnitLiteral = function(test) {
-    var result = parser.parseExpression("()");
+    var result = parser.parse(parsing.expression, "()");
     assertIsSuccessWithValue(test, result, nodes.unit());
     test.done();
 };
 
 exports.canParseVariableReference = function(test) {
-    var result = parser.parseExpression("blah");
+    var result = parser.parse(parsing.expression, "blah");
     assertIsSuccessWithValue(test, result, nodes.variableReference("blah"));
     test.done();
 };
 
 exports.canParseShortLambdaExpressionWithNoArguments = function(test) {
-    var result = parser.parseExpression("()=>true");
+    var result = parser.parse(parsing.expression, "()=>true");
     var expected = nodes.shortLambda([], options.none, nodes.boolean(true));
     assertIsSuccessWithValue(test, result, expected);
     test.done();
 };
 
 exports.canParseShortLambdaExpressionWithExplicitReturnType = function(test) {
-    var result = parser.parseExpression("() : Boolean => true");
+    var result = parser.parse(parsing.expression, "() : Boolean => true");
     var expected = nodes.shortLambda([], some(nodes.ref("Boolean")), nodes.boolean(true));
     assertIsSuccessWithValue(test, result, expected);
     test.done();
 };
 
 exports.canParseShortLambdaExpressionWithFormalArguments = function(test) {
-    var result = parser.parseExpression("(name: String, age: Age) => true");
+    var result = parser.parse(parsing.expression, "(name: String, age: Age) => true");
     var expected = nodes.shortLambda([
         nodes.formalArgument("name", nodes.ref("String")),
         nodes.formalArgument("age", nodes.ref("Age"))
@@ -59,7 +61,7 @@ exports.canParseShortLambdaExpressionWithFormalArguments = function(test) {
 };
 
 exports.missingShortLambdaBodyIsReportedRatherThanGenericFailureToParseAnExpression = function(test) {
-    var result = parser.parseExpression("() => ");
+    var result = parser.parse(parsing.expression, "() => ");
     assertIsError(test, result, {
         errors: [errors.error({
             expected: "expression",
@@ -70,7 +72,7 @@ exports.missingShortLambdaBodyIsReportedRatherThanGenericFailureToParseAnExpress
 };
 
 exports.lambdaIsRightAssociative = function(test) {
-    var result = parser.parseExpression("()=>()=>true");
+    var result = parser.parse(parsing.expression, "()=>()=>true");
     var expected = nodes.shortLambda([], options.none,
         nodes.shortLambda([], options.none,
             nodes.boolean(true)
@@ -81,7 +83,7 @@ exports.lambdaIsRightAssociative = function(test) {
 };
 
 exports.whitespaceIsIgnored = function(test) {
-    var result = parser.parseExpression("() =>\n\ttrue");
+    var result = parser.parse(parsing.expression, "() =>\n\ttrue");
     var expected = nodes.shortLambda([], options.none, nodes.boolean(true));
     assertIsSuccessWithValue(test, result, expected);
     test.done();
