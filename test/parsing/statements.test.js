ee740e4aa193b841aeba8757b17617fee642e5c2
Michael Williamson <mike@zwobble.org>
2012-02-21 21:07:06 +0000

    Add parsing for return statements

---
+++ test/parsing/statements.test.js
@@ -0,0 +1,19 @@
+var options = require("options");
+var some = options.some;
+
+var errors = require("lop").errors;
+var parsingTesting = require("lop").testing;
+var assertIsSuccessWithValue = parsingTesting.assertIsSuccessWithValue;
+var assertIsFailure = parsingTesting.assertIsFailure;
+var assertIsError = parsingTesting.assertIsError;
+
+var nodes = require("../../lib/nodes");
+var parsing = require("../../lib/parsing");
+
+var parser = new parsing.Parser();
+
+exports.canParseReturnStatement = function(test) {
+    var result = parser.parse(parsing.statement, "return true;");
+    assertIsSuccessWithValue(test, result, nodes.return(nodes.boolean(true)));
+    test.done();
+};
