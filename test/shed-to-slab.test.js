332e3434e28fa1ffa2104a9a9949a74f7f8ebbe5
Michael Williamson <mike@zwobble.org>
2012-02-22 21:44:26 +0000

    Store source of slab nodes

--- test/shed-to-slab.test.js
+++ test/shed-to-slab.test.js
@@ -4,71 +4,102 @@ var shedToSlab = require("../lib/shed-to-slab");
 
 exports.shedBooleansAreConvertedToSlabBooleans = function(test) {
     var original = shed.boolean(true);
-    test.deepEqual(slab.boolean(true), shedToSlab.translate(original));
+    test.deepEqual(
+        slab.boolean(true, original),
+        shedToSlab.translate(original)
+    );
     test.done();
 };
 
 exports.shedUnitIsConvertedToSlabUnit = function(test) {
     var original = shed.unit();
-    test.deepEqual(slab.unit(), shedToSlab.translate(original));
+    test.deepEqual(slab.unit(original), shedToSlab.translate(original));
     test.done();
 };
 
 exports.shedVariableReferenceIsConvertedToSlabVariableReference = function(test) {
     var original = shed.ref("blah");
-    test.deepEqual(slab.ref("blah"), shedToSlab.translate(original));
+    test.deepEqual(slab.ref("blah", original), shedToSlab.translate(original));
     test.done();
 };
 
 exports.shedFormalArgumentIsConvertedToSlabFormalArgument = function(test) {
-    var original = shed.formalArgument("name", shed.ref("String"));
-    test.deepEqual(slab.formalArgument("name", slab.ref("String")), shedToSlab.translate(original));
+    var reference = shed.ref("String");
+    var original = shed.formalArgument("name", reference);
+    test.deepEqual(
+        slab.formalArgument("name", slab.ref("String", reference), original),
+        shedToSlab.translate(original)
+    );
     test.done();
 };
 
 exports.shedFormalArgumentsIsConvertedToSlabFormalArguments = function(test) {
-    var original = shed.formalArguments([shed.formalArgument("name", shed.ref("String"))]);
+    var reference = shed.ref("String");
+    var formalArgument = shed.formalArgument("name", reference);
+    var original = shed.formalArguments([formalArgument]);
     test.deepEqual(
-        slab.formalArguments([slab.formalArgument("name", slab.ref("String"))]),
+        slab.formalArguments(
+            [
+                slab.formalArgument(
+                    "name",
+                    slab.ref("String", reference),
+                    formalArgument
+                )
+            ],
+            original
+        ),
         shedToSlab.translate(original)
     );
     test.done();
 };
 
 exports.shedReturnIsConvertedToSlabReturn = function(test) {
-    var original = shed.return(shed.ref("blah"));
-    test.deepEqual(slab.return(slab.ref("blah")), shedToSlab.translate(original));
+    var reference = shed.ref("blah");
+    var original = shed.return(reference);
+    test.deepEqual(
+        slab.return(slab.ref("blah", reference), original),
+        shedToSlab.translate(original)
+    );
     test.done();
 };
 
 exports.shedBlockIsConvertedToSlabBlock = function(test) {
-    var original = shed.block([
-        shed.return(shed.ref("blah"))
-    ]);
+    var shedReference = shed.ref("blah");
+    var shedReturn = shed.return(shedReference);
+    var original = shed.block([shedReturn]);
     test.deepEqual(
         slab.block([
-            slab.return(slab.ref("blah"))
-        ]),
+            slab.return(slab.ref("blah", shedReference), shedReturn)
+        ], original),
         shedToSlab.translate(original)
     );
     test.done();
 };
 
 exports.shedLongLambdaIsConvertedToSlabLambda = function(test) {
-    var original = shed.lambda(
-        shed.formalArguments([shed.formalArgument("name", shed.ref("String"))]),
-        shed.ref("Boolean"),
-        shed.block([
-            shed.return(shed.ref("blah"))
-        ])
-    );
+    var stringReference = shed.ref("String");
+    var formalArgument = shed.formalArgument("name", stringReference);
+    var formalArguments = shed.formalArguments([formalArgument]);
+    var booleanReference = shed.ref("Boolean");
+    var reference = shed.ref("blah");
+    var returnStatement = shed.return(reference);
+    var block = shed.block([returnStatement]);
+    var original = shed.lambda(formalArguments, booleanReference, block);
+    
     test.deepEqual(
         slab.lambda(
-            slab.formalArguments([slab.formalArgument("name", slab.ref("String"))]),
-            slab.ref("Boolean"),
+            slab.formalArguments([
+                slab.formalArgument(
+                    "name",
+                    slab.ref("String", stringReference),
+                    formalArgument
+                )
+            ], formalArguments),
+            slab.ref("Boolean", booleanReference),
             slab.block([
-                slab.return(slab.ref("blah"))
-            ])
+                slab.return(slab.ref("blah", reference), returnStatement)
+            ], block),
+            original
         ),
         shedToSlab.translate(original)
     );
@@ -76,18 +107,26 @@ exports.shedLongLambdaIsConvertedToSlabLambda = function(test) {
 };
 
 exports.shedShortLambdaIsConvertedToSlabLambda = function(test) {
-    var original = shed.lambda(
-        shed.formalArguments([shed.formalArgument("name", shed.ref("String"))]),
-        shed.ref("Boolean"),
-        shed.ref("blah")
-    );
+    var stringReference = shed.ref("String");
+    var formalArgument = shed.formalArgument("name", stringReference);
+    var formalArguments = shed.formalArguments([formalArgument]);
+    var booleanReference = shed.ref("Boolean");
+    var reference = shed.ref("blah");
+    var original = shed.lambda(formalArguments, booleanReference, reference);
     test.deepEqual(
         slab.lambda(
-            slab.formalArguments([slab.formalArgument("name", slab.ref("String"))]),
-            slab.ref("Boolean"),
+            slab.formalArguments([
+                slab.formalArgument(
+                    "name",
+                    slab.ref("String", stringReference),
+                    formalArgument
+                )
+            ], formalArguments),
+            slab.ref("Boolean", booleanReference),
             slab.block([
-                slab.return(slab.ref("blah"))
-            ])
+                slab.return(slab.ref("blah", reference), reference)
+            ], reference),
+            original
         ),
         shedToSlab.translate(original)
     );
