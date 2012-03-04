3c15127f46daf6ec16315c51a2573874f5b0c91c
Michael Williamson <mike@zwobble.org>
2012-03-04 19:01:39 +0000

    Add writing for JavaScript variable reference

--- test/javascript/writer.test.js
+++ test/javascript/writer.test.js
@@ -4,6 +4,9 @@ var writer = require("../../lib/javascript/writer");
 exports.writesString = function(test) {
     assertJavaScriptWriter(test, js.string("Blah"), '"Blah"');
 };
+exports.writesVariableReference = function(test) {
+    assertJavaScriptWriter(test, js.ref("print"), 'print');
+};
 
 var assertJavaScriptWriter = function(test, javaScriptNode, expectedString) {
     test.deepEqual(writer.write(javaScriptNode), expectedString);
