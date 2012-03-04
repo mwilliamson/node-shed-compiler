2e6abf1f7781696a5846e3e9833f5699c6bee2c9
Michael Williamson <mike@zwobble.org>
2012-03-04 19:04:02 +0000

    Add writing for JavaScript function calls

--- test/javascript/writer.test.js
+++ test/javascript/writer.test.js
@@ -4,8 +4,17 @@ var writer = require("../../lib/javascript/writer");
 exports.writesString = function(test) {
     assertJavaScriptWriter(test, js.string("Blah"), '"Blah"');
 };
+
 exports.writesVariableReference = function(test) {
     assertJavaScriptWriter(test, js.ref("print"), 'print');
+};
+
+exports.writesFunctionCallWithArguments = function(test) {
+    assertJavaScriptWriter(
+        test,
+        js.call(js.ref("print"), [js.ref("a"), js.ref("b")]),
+        'print(a, b)'
+    );
 };
 
 var assertJavaScriptWriter = function(test, javaScriptNode, expectedString) {
