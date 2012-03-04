40a8b0b152c5ba5711f40fa0bb2bff93cfcce8b5
Michael Williamson <mike@zwobble.org>
2012-03-04 16:38:38 +0000

    Add slab to js generation for variable references

--- test/code-generation/micro-javascript.test.js
+++ test/code-generation/micro-javascript.test.js
@@ -3,11 +3,18 @@ var js = require("../../lib/code-generation/javascript-nodes");
 
 var codeGenerator = require("../../lib/code-generation/micro-javascript");
 
+var slabString = slab.string("Blah");
+var jsString = js.string("Blah", slabString);
+
+var slabReference = slab.ref("blah");
+var jsReference = js.ref("blah", slabReference);
+
 exports.slabStringLiteralIsConvertedToJavaScriptStringLiteral = function(test) {
-    var slabString = slab.string("Blah");
-    var jsString = js.string("Blah", slabString);
-    
     assertTranslation(test, slabString, jsString);
+};
+
+exports.slabReferenceIsConvertedToJavaScriptReference = function(test) {
+    assertTranslation(test, slabReference, jsReference);
 };
 
 var assertTranslation = function(test, slab, expectedJavaScript) {
