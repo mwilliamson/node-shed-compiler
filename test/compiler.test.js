d9141e71c6a47cf9e1eec6f4818de3640f6c9756
Michael Williamson <mike@zwobble.org>
2012-03-06 21:44:42 +0000

    Add full compiler that correctly compiles test program

---
+++ test/compiler.test.js
@@ -0,0 +1,48 @@
+var util = require("util");
+var fs = require("fs");
+var path = require("path");
+var child_process = require("child_process");
+
+var temp = require("temp");
+
+var Compiler = require("../lib/compiler").Compiler;
+
+var compiler = new Compiler();
+
+var testRoot = path.join(__dirname, "../test-programs");
+
+fs.readdirSync(testRoot).forEach(function(testPath) {
+    var testDirectory = path.join(testRoot, testPath);
+    exports[testPath] = function(test) {
+        fs.readFile(path.join(testDirectory, "test.json"), function(err, testJson) {
+            test.ifError(err);
+            var testDescription = JSON.parse(testJson);
+            var mainTestFilePath = path.join(testDirectory, testDescription.main);
+            fs.readFile(mainTestFilePath, "utf8", function(err, mainShedString) {
+                test.ifError(err);
+                var compiledJavaScript = compiler.compileToString({
+                    string: mainShedString
+                });
+                
+                console.log("!!" + compiledJavaScript + "!!");
+                
+                temp.open(null, function(err, tempFile) {
+                    test.ifError(err);
+                    fs.writeFile(tempFile.path, compiledJavaScript, function(err) {
+                        test.ifError(err);
+                        
+                        var command = util.format("node %s", path.resolve(tempFile.path));
+                        child_process.exec(command, function(err, stdout, stderr) {
+                            test.ifError(err);
+                            test.equal("", stderr);
+                            test.equal(testDescription.expected.stdout, stdout);
+                            test.done(); 
+                        });
+                    });
+                });
+            });
+        });
+    };
+});
+
+
