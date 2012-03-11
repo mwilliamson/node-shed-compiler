b644c4d5457d0936e6a1b7a79461b58af2791a07
Michael Williamson <mike@zwobble.org>
2012-03-11 17:51:54 +0000

    Add translation from Slab to JavaScript for ifs and booleans

--- test/code-generation/micro-javascript.test.js
+++ test/code-generation/micro-javascript.test.js
@@ -5,6 +5,9 @@ var js = require("../../lib/javascript/nodes");
 
 var codeGenerator = require("../../lib/code-generation/micro-javascript");
 
+var slabBoolean = slab.boolean(true);
+var jsBoolean = js.boolean(true, slabBoolean);
+
 var slabString = slab.string("Blah");
 var jsString = js.string("Blah", slabString);
 
@@ -22,6 +25,10 @@ var jsExpressionStatement = js.expressionStatement(jsFunctionCall, slabExpressio
 
 var slabReturn = slab.return(slabNumber);
 var jsReturn = js.return(jsNumber, slabReturn);
+
+exports.slabBooleanLiteralIsConvertedToJavaScriptBooleanLiteral = function(test) {
+    assertTranslation(test, slabBoolean, jsBoolean);
+};
 
 exports.slabStringLiteralIsConvertedToJavaScriptStringLiteral = function(test) {
     assertTranslation(test, slabString, jsString);
@@ -65,6 +72,21 @@ exports.slabExpressionStatementIsConvertedToJavaScriptExpressionStatement = func
 exports.slabValIsConvertedToJavaScriptVar = function(test) {
     var slabVal = slab.val("coins", options.none, slabNumber);
     assertTranslation(test, slabVal, js.var("coins", jsNumber, slabVal));
+};
+
+exports.slabIfStatementIsConvertedToJavaScriptIfStatement = function(test) {
+    var slabIf = slab.if([
+        {condition: slabBoolean, body: slab.block([slabReturn])},
+        {body: slab.block([slabExpressionStatement])}
+    ]);
+    var jsIf = js.if(
+        [
+            {condition: jsBoolean, body: [jsReturn]},
+            {body: [jsExpressionStatement]}
+        ],
+        slabIf
+    );
+    assertTranslation(test, slabIf, jsIf);
 };
 
 exports.slabModuleIsConvertedToJavaScriptFunctionThatsImmediatelyCalled = function(test) {
